c840b7554510e2aa781f65966c6ce41ef389ac0a
jdavidson <jdavidson@rsglab.com>
2016-01-20 10:08:16 -0500

    fix prose urls

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp'),
-	ghPages = require('gulp-gh-pages');
+	ghPages = require('gulp-gh-pages')
+	replace = require('gulp-replace');
 
+// deploys to gh-pages
 gulp.task('deploy', function() {
 		return gulp.src('./build/**/*')
 			.pipe(ghPages());
 	})
 
+// fix for prose media destination
 gulp.task('media', function() {
 	return gulp.src(['./media/**/*'])
 			.pipe(gulp.dest('./build'));
+	})
+
+// fix for prose image urls.
+gulp.task('prose-fix', function() {
+	return gulp.src(['./src/**/*.md'])
+			.pipe(replace('{{site.baseurl}}', ''))
+			.pipe(gulp.dest('./src'))
 	})
