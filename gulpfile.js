1e3d0e84d930c2932503724e398113a651ca9006
jdavidson <jdavidson@rsglab.com>
2016-01-20 00:16:59 -0500

    add media transfer

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,8 @@ gulp.task('deploy', function() {
 		return gulp.src('./build/**/*')
 			.pipe(ghPages());
 	})
+
+gulp.task('media', function() {
+	return gulp.src(['./media/**/*'])
+			.pipe(dest('./build'));
+	})
