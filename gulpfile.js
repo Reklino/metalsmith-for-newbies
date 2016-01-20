784a1af03c4908b745071ca7792b6c2f3eeee37e
jdavidson <jdavidson@rsglab.com>
2016-01-20 10:46:14 -0500

    ugh

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,6 @@ gulp.task('media', function() {
 // fix for prose image urls.
 gulp.task('prose-fix', function() {
 	return gulp.src(['./src/**/*.md'])
-			.pipe(replace('{{site.baseurl}}', 'metalsmith-for-newbies/'))
+			.pipe(replace('{{site.baseurl}}', ''))
 			.pipe(gulp.dest('./src'))
 	})
